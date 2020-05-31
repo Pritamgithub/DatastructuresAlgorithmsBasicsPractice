02997dec52cc08b452795b6f608284c4efed61ab
Pritam Chavhan <pritam0263@gmail.com>
2020-05-31 13:08:19 -0400

    Update AnagramBasicWay_Sort_And_Compare.java
    +Added break condition

--- src/Strings/AnagramBasicWay_Sort_And_Compare.java
+++ src/Strings/AnagramBasicWay_Sort_And_Compare.java
@@ -17,20 +17,22 @@ public class AnagramBasicWay_Sort_And_Compare {
 		// Method 1.
 		// Sort both the strings
 		// compare the sorted Strings
-		String s1 = "Listen";
-		String s2 = "Triangle";
+		String s1 = "ab";
+		String s2 = "ba";
 		checkAnagram(s1, s2);
 	}
 	private static void checkAnagram(String s1, String s2) {
-		char s3[] = s1.toCharArray();
-		char s4[] = s2.toCharArray();
-		Arrays.sort(s3);
-		System.out.println(s3);
-		Arrays.sort(s4);
-		System.out.println(s4);
-		// compare two strings which are in char arrays
-		if (Arrays.equals(s3, s4)) {
-			System.out.println("Both the strings are Anagrams");
+		if (s1.length() == s2.length()) {
+			char s3[] = s1.toCharArray();
+			char s4[] = s2.toCharArray();
+			Arrays.sort(s3);
+			System.out.println(s3);
+			Arrays.sort(s4);
+			System.out.println(s4);
+			// compare two strings which are in char arrays
+			if (Arrays.equals(s3, s4)) {
+				System.out.println("Both the strings are Anagrams");
+			}
 		} else
 			System.out.println("Both the strings are NOT Anagrams");
 
