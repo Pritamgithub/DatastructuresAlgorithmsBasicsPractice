5c53f2833d5d8c84e231e66e1b60ce625727ac69
Pritam Chavhan <pritam0263@gmail.com>
2024-05-26 02:55:08 +0000

    Added code that can add muliply or divide the elements of array

---
+++ src/May 2024/AddArrayElements.java
@@ -0,0 +1,17 @@
+public class AddArrayElements {
+
+//create and declare an array of ints
+// iterate through the array
+//add the sum of the ints in a variable and display the total of the same
+public static void main(String[] args) {
+    int sumofArray=0;
+int a[]=new int[]{1,4,5,6};
+for (int i=0;i<a.length;i++)
+{
+    sumofArray += a[i];
+}
+System.out.println("The total of the array elements "+ sumofArray);
+}
+
+// Same way in order to multiple the arrays 
+}
