43bb6fab12b936f48e13c1b0e6723c3a13ef1769
Pritam Chavhan <pritam0263@gmail.com>
2024-05-26 13:23:47 +0000

    Added more learnings and understanding on Array

--- src/May 2024/AddArrayElements.java
+++ src/May 2024/AddArrayElements.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
 public class AddArrayElements {
 
 //create and declare an array of ints
@@ -11,7 +14,47 @@ for (int i=0;i<a.length;i++)
     sumofArray += a[i];
 }
 System.out.println("The total of the array elements "+ sumofArray);
-}
 
 // Same way in order to multiple the arrays 
+
+//===============================================
+//new problem
+// convert an array as list
+//first declare an array and then just use that array 
+
+int b[]= {1,4,5,6,7,4,5,6};
+System.out.println("Integer Array as List: "
++ Arrays.asList(Arrays.toString(b)));
+
+/// New Problem
+//Search an element in an array
+
+int c[]={1,4,5,6,7,4,5,6};
+
+Arrays.sort(c);
+
+System.out.println("Found at index" + Arrays.binarySearch(c, 2));
+
+//Similarly we can search using binary search with (array, start index, end index, comparator)
+
+System.out.println(" found on this index "+Arrays.binarySearch(c, 0, c.length, 5));
+// let's compare two arrays 
+
+System.out.println(" this is the comparison of two arrays "+Arrays.compare(c, b));
+
+// returns -1 if the arrays are same and else if not then returns 1
+
+
+//copy one array or say length increment
+
+Arrays.copyOf(c, 10); // will append the new index with 0's
+
+
+//parallel sort means sorting an array based using the multithreading approach
+
+Arrays.parallelSort(c);
+System.out.println(Arrays.toString(c));
+
+}
+
 }
--- src/May 2024/ArraysPractice.java
+++ src/May 2024/ArraysPractice.java
@@ -8,6 +8,7 @@
  * 
  */
 
+
 public class ArraysPractice {
 
 	/**
@@ -17,18 +18,18 @@ public class ArraysPractice {
 	 */
 	public static void main(String[] args) {
 	//StudentasArray class is created as an object class which has two variables int roll_number and string name; Can be accessed in this way intialize, declared	
-	StudentasArray[] arr = new StudentasArray[6];
-		arr[0]= new StudentasArray(1, "null");
-		arr[1]= new StudentasArray(2, "null1");
-		arr[2]= new StudentasArray(3, "null2");
-		arr[3]= new StudentasArray(4, "null3");
-		arr[4]= new StudentasArray(5, "null4");
-		arr[5]= new StudentasArray(6, "null5");
+	// StudentasArray[] arr = new StudentasArray[7];
+	// 	arr[0]= new StudentasArray(1, "null");
+	// 	arr[1]= new StudentasArray(2, "null1");
+	// 	arr[2]= new StudentasArray(3, "null2");
+	// 	arr[3]= new StudentasArray(4, "null3");
+	// 	arr[4]= new StudentasArray(5, "null4");
+	// 	arr[5]= new StudentasArray(6, "null5");
 		
-		for(StudentasArray Students: arr)
-		{
-			System.out.println(Students.roll_number);
-		}
+	// 	for(StudentasArray Students: arr)
+	// 	{
+	// 		System.out.println(Students.roll_number);
+	// 	}
 		
 	//arr[0]= new StudentasArray (1,"pritam");
 	StudentasArray[] arr1 = { 
